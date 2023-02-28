c64f98dc3c8f6d59f61f0f5fff5aa18a8419e0b9
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2023-02-28 19:55:38 +0500

    added index.ts

---
+++ Number_Guessing_Game/index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const systemGeneratedNo = Math.floor(Math.random() * 100);
+async function askQuestion() {
+    let response = await inquirer
+        .prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: "Enter your guess here (Select a number between 1 to 100): ",
+            validate: function (input) {
+                if (isNaN(input)) {
+                    console.log("Please enter a valid integer number");
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+    ]);
+    if (response.name === systemGeneratedNo) {
+        console.log("You guess a right number :)");
+    }
+    else {
+        console.log("Not a right number");
+    }
+}
+async function askAgain() {
+    do {
+        await askQuestion();
+        var userInput = await inquirer
+            .prompt({
+            type: "input",
+            name: "restart",
+            message: "Do you want to continue. Press Y or N"
+        });
+    } while (userInput.restart === 'Y' || userInput.restart === 'y' || userInput.restart === 'yes' || userInput.restart === 'YES');
+}
+askAgain();

a7a85166199f98db36788c5447ecb9e9d52f513b
Malik Zohaib Zahid <zohaibzahid112233@gmail.com>
2023-03-09 20:53:22 +0500

    added index.ts

--- Countdown_Timer/index.js
+++ Countdown_Timer/index.js
@@ -1,12 +1,65 @@
-let currentTime = Date.now();
-let days;
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-let months;
-months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec'];
-let dateObj = new Date(currentTime);
-let currentDate = dateObj.getDate();
-let currentDay = dateObj.getDay() - 1;
-let currentMonth = dateObj.getMonth();
-let currentYear = dateObj.getFullYear();
-console.log(`Day -> ${days[currentDay]}, Date -> ${currentDate}, Month -> ${months[currentMonth]}, Year -> ${currentYear}`);
-export {};
+import inquirer from "inquirer";
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+async function welcome() {
+    let title;
+    title = "Welcome To Countdown Timer App - Created By Zohaib :)";
+    let welcomingTitle = chalkAnimation.neon(title);
+    await sleep();
+    welcomingTitle.stop();
+}
+await welcome();
+async function App() {
+    let question = await inquirer.prompt([
+        {
+            type: "number",
+            name: "user",
+            message: "Select a timer in seconds",
+            validate: function (input) {
+                if (isNaN(input)) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+    ]);
+    const updateTime = () => {
+        console.clear();
+        if (question.user > 0) {
+            let remainingTime;
+            remainingTime = `${question.user} seconds remaining`;
+            console.log(chalk.greenBright(remainingTime));
+            setTimeout(updateTime, 1000);
+            question.user = question.user - 1;
+        }
+        else {
+            let remainingTime;
+            remainingTime = `Time Finished`;
+            console.log(chalk.red(remainingTime));
+        }
+    };
+    console.clear();
+    setTimeout(updateTime);
+}
+await App();
+async function startAgain() {
+    do {
+        await App();
+        var askAgain = await inquirer.prompt([
+            {
+                type: "input",
+                name: "userFeedback",
+                message: "Do you want to use this app again ?"
+            }
+        ]);
+    } while (askAgain.userFeedback === 'Yes' || askAgain.userFeedback === 'YES' || askAgain.userFeedback === 'yes' || askAgain.userFeedback === 'Y' || askAgain.userFeedback === 'y');
+    console.log("Thanks for using my app.");
+}
+await startAgain();
--- Countdown_Timer/index.ts
+++ Countdown_Timer/index.ts
@@ -1,15 +1,83 @@
-let currentTime = Date.now();
+import inquirer from "inquirer";
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
 
-let days: string [];
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-let months: string [];
-months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec'];
+interface ansType {
+    user: number;
+}
 
-let dateObj = new Date(currentTime);
-let currentDate = dateObj.getDate();
-let currentDay = dateObj.getDay() - 1;
-let currentMonth = dateObj.getMonth();
-let currentYear = dateObj.getFullYear()
+interface ansType2{
+    userFeedback: string;
+}
 
-console.log(`Day -> ${days[currentDay]}, Date -> ${currentDate}, Month -> ${months[currentMonth]}, Year -> ${currentYear}`);
+const sleep = () => {
+    return new Promise((res)=>{
+        setTimeout(res, 3000);
+    })
+}
+
+async function welcome(){
+    let title: string;
+    title = "Welcome To Countdown Timer App - Created By Zohaib :)";
+
+    let welcomingTitle = chalkAnimation.neon(title);
+
+    await sleep();
+    welcomingTitle.stop();
+}
+
+await welcome();
+
+async function App(){
+    let question: ansType = await inquirer.prompt([
+        {
+            type: "number",
+            name: "user",
+            message: "Select a timer in seconds",
+            validate: function(input){
+                if(isNaN(input)){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }]);
+
+        const updateTime = () => {
+            console.clear();
+            if(question.user > 0){
+                let remainingTime: string;
+                remainingTime = `${question.user} seconds remaining`
+                console.log(chalk.greenBright(remainingTime));
+                setTimeout(updateTime, 1000);
+                question.user = question.user - 1;
+            } 
+            else {
+                let remainingTime: string;
+                remainingTime = `Time Finished`;
+                console.log(chalk.red(remainingTime));
+            }
+        };
+        console.clear();
+        setTimeout(updateTime);
+    }
+
+await App();
+
+  async function startAgain(){
+    do{
+        await App();
+
+        var askAgain: ansType2 = await inquirer.prompt([
+            {
+                type: "input", 
+                name: "userFeedback",
+                message: "Do you want to use this app again ?"
+            }
+        ])
+    } while (askAgain.userFeedback === 'Yes' || askAgain.userFeedback === 'YES' || askAgain.userFeedback === 'yes' || askAgain.userFeedback === 'Y' || askAgain.userFeedback === 'y')
+    console.log("Thanks for using my app.");
+}
+
+await startAgain();
